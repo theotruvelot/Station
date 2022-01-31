492d7a149bde6bdeedd99ee4a8704c6defc4613b
p2acoo <78920246+p2acoo@users.noreply.github.com>
2022-01-31 09:02:15 +0100

    added front + func

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,10 @@
 from flask import Flask
-from flask_restx import Resource, Api, fields, reqparse
+from flask_restx import Resource, Api
 import logging
 from datetime import date
 import pymysql
-
+import string
+import random
 # récuperation de la date pour les logs.
 today = date.today()
 d1 = today.strftime("%d%m%Y")
@@ -22,6 +23,7 @@ try:
                              password='root',
                              database='weatherstation')
     logging.info("Database connection successful")
+    db.close()
 except pymysql.Error as err:
     logging.error(err)
     print(err)
@@ -40,13 +42,23 @@ class Ping(Resource):
 @api.route("/sondes")
 class Sondes(Resource):
         def get(self):
-            sql = "select SON_ID, SON_STATUS,   from sondes ;"
+            sql = "select SON_ID, SON_STATUS  from sondes ;"
             with db.cursor(pymysql.cursors.DictCursor) as cursor:
                 try:
+                    db.connect()
                     cursor.execute(sql)
                     result = cursor.fetchall()
-                    return {'Sondes':result}, 200
+                    sond_list = []
+                    for list in result:
+                            print(list)
+                            sond_list.append({
+                            "sonde_id":list['SON_ID'],
+                            "sonde_status":list['SON_STATUS']
+                            })
+                    db.close()
+                    return sond_list, 200
                 except db.Error as err:
+                    logging.error(err)
                     print(err)
 
 # récuperation des données envoyé par les sondes, vérification de sa clé, envoie dans la db
@@ -59,12 +71,14 @@ class Temp(Resource):
         sqllast = "UPDATE SONDES SET SON_LAST_MESURE=now() where SON_ID=%s"
         with db.cursor(pymysql.cursors.DictCursor) as cursor:
             try:
+                db.connect
                 # récupération de la clé dans la db 
                 cursor.execute(sql_sondekey, idsonde)
                 sondks = cursor.fetchone()
                 # gestion du cas ou l'id de la sonde est inconnue 
                 if str(sondks['SON_KEY']) == None:
                     logging.warning("A probe wan't to inject with a wrong ID")
+                    db.close()
                     return{'Error': 'Check your probe ID'}, 403
                 else: 
                 # vérification si la clé reçu correspond bien à la clé de la sonde
@@ -76,20 +90,23 @@ class Temp(Resource):
                             db.commit()
                             last_id = cursor.lastrowid
                             logging.info('Temp injected with probe ID : ' + idsonde + ', temp : ' + temp )
+                            db.close()
                             return {'Temp injected with ID':str(last_id)}, 200
                         except pymysql.Error as err:
+                            db.close()
                             logging.error(err)
                             return {'Error in temp injection'}, 500
                     else:
+                        db.close()
                         logging.warning("Sonde ID : " + idsonde + " want to inject with the wrong key or the status of probe = 0")
                         return{'Error': 'Check your API key or the status of the probe is down'}, 403
             except (pymysql.Error):
+                db.close()
                 logging.error("Error for ckecking probe ID")
                 return{'Error': 'for checking probe ID'}, 500
 
-
-
-@api.route("/admin/set/<string:idsonde>/<string:status>")
+# mise a jour du status d'une sonde
+@api.route("/admin/set/<string:idsonde>/<string:status>", methods=["PUT"])
 class admin(Resource):
     def put(self, idsonde, status):
         #  commande pour la récupération du status / vérification l'existance de l'ID
@@ -98,24 +115,107 @@ class admin(Resource):
         sql2 = "UPDATE SONDES SET SON_STATUS=%s WHERE SON_ID=%s"
         with db.cursor(pymysql.cursors.DictCursor) as cursor:
             try:
+                db.connect()
                 cursor.execute(sql1, idsonde)
                 sondverif = cursor.fetchone()
                 # vérification si la sonde existe / si son status n'est pas = à celui demandé
                 if sondverif == None or sondverif['SON_STATUS'] == str(status):
                     logging.warning("Someone want change status of unknow probe")
+                    db.close()
                     return{'Error': 'Check your probe ID or the status or the status of the probe is already set'}, 403
                 else :
                     # envoie de la commande de mise a jour
                     cursor.execute(sql2, (status, idsonde))
                     db.commit()
+                    db.close()
                     logging.info("Probe status with probe ID : " + idsonde + " was updated to : " + status)
                     return{'Probe updated with ID':str(idsonde), 'and status':str(status)}               
             except pymysql.Error as err:
+                db.close()
                 logging.error(err)
                 return {'Error in update probe status'}, 500
+@api.route("/sonde/create/<string:useid>/<string:location>", methods=["POST"])
+class sonde(Resource):
+    def post(self, location, useid):
+        length_of_string = 50
+        key = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(length_of_string))
+        print(key)
+        sql = "INSERT INTO SONDES (SON_LOCATION, SON_KEY) VALUES (%s, %s)"
+        sql1 = "INSERT INTO POSSEDE (SON_ID, USE_ID) VALUES (%s, %s)"
+        with db.cursor(pymysql.cursors.DictCursor) as cursor:
+            try:
+                db.connect()
+                cursor.execute(sql, (location, key))
+                last_id = cursor.lastrowid
+                cursor.execute(sql1, (last_id, useid))
+                db.commit()
+                return {'Sonde created with id:':str(last_id)}, 200
+            except pymysql.Error as err:
+                db.close()
+                logging.error(err)
+                return{'Erreur dans la création de la sonde'}, 500
 
 
-
+@api.route("/user/sondes/<string:userid>", methods=["GET"])
+class usondes(Resource):
+    def get(self, userid):
+        sql = "SELECT POSSEDE.SON_ID, SONDES.SON_STATUS, SONDES.SON_LAST_MESURE, SONDES.SON_LOCATION , SONDES.SON_KEY  FROM POSSEDE INNER JOIN SONDES ON POSSEDE.SON_ID = SONDES.SON_ID  WHERE USE_ID = %s"
+        sondes_list = []
+        with db.cursor(pymysql.cursors.DictCursor) as cursor:
+                try:
+                    db.connect()
+                    cursor.execute(sql, userid)
+                    sondes = cursor.fetchall()
+                    if sondes == None:
+                        db.close()
+                        logging.warning("Connais pas")
+                    else:
+                        db.connect()
+                    for releve in sondes:
+                        print(releve)
+                        sondes_list.append({
+                                "sonde_id":releve['SON_ID'],
+                                "sonde_status":str(releve['SON_STATUS']),
+                                "sonde_lastmes":str(releve['SON_LAST_MESURE']),
+                                "sonde_loca":releve['SON_LOCATION'],
+                                "sonde_key":releve['SON_KEY']
+                        })
+                        logging.info("recuperation des temps de la sondes %s", sondes_list)
+                    return sondes_list, 200
+                except pymysql.Error as err:
+                    db.close()
+                    logging.error(err)
+                    return{'Erreur dans la récupération des sondes'}, 500
+# récupere tout les temp prisent par une sonde 
+@api.route("/sonde/data/<string:idsonde>", methods=["GET"])
+class temp(Resource):
+    def get(self, idsonde):
+        releve_list = []
+        sql = 'SELECT * FROM MESURES WHERE SON_ID = %s'
+        with db.cursor(pymysql.cursors.DictCursor) as cursor:
+            try:
+                db.connect()
+                cursor.execute(sql, idsonde)
+                sondtemp = cursor.fetchall()
+                if sondtemp == None:
+                    db.close()
+                    logging.warning("Connais pas")
+                else:
+                    db.connect()
+                    for releve in sondtemp:
+                        print(releve)
+                        releve_list.append({
+                                "releve_id":releve['MES_ID'],
+                                "releve_date":str(releve['MES_DATE']),
+                                "releve_temp":str(releve['MES_TEMP']),
+                                "releve_sonde":releve['SON_ID']  
+                        })
+                        logging.info("recuperation des temps de la sondes %s", idsonde)
+                    return releve_list, 200
+            except pymysql.Error as err:
+                db.close()
+                logging.error(err)
+                return{'Error db'}, 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5010', debug=True)
